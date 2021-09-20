79726b0a7be459e94489d8380602fb35df2a6688
Tyler S <42685071+Tyler-Seliber@users.noreply.github.com>
2021-09-19 20:41:09 -0400

    Finished HW 2

--- HW 2/HW2.py
+++ HW 2/HW2.py
@@ -18,7 +18,29 @@ from math import floor    # E.g., floor(5.3) -> 5, floor(3.9) -> 3
 #         0: Saturday, 1: Sunday, 2: Monday, ..., 6: Friday  
 ######################################################################
 def getWeekday(date):
-    pass    # TODO: replace this line with your code (multiple lines)
+    # Parse date into year, month, and day values
+    
+    # Get day, which is last two digits of date
+    day = date % 100
+    date //= 100
+    # Get month, which is last two digits of date
+    month = date % 100
+    date //= 100
+    # Get year, which is now equal to date
+    year = date
+
+    # Convert January to 13 and February to 14, and subtract 1 from year
+    if month == 1:
+        month = 13
+        year -= 1
+    elif month == 2:
+        month = 14
+        year -= 1
+ 
+
+    weekday = (day +floor((13 * (month + 1) / 5)) + (year % 100) + floor((year % 100) / 4) + floor((floor((year / 100)) / 4)) + (5 * floor((year / 100)))) % 7
+
+    return weekday
 
 ######################################################################
 # Task 2: Create two functions, an encoder and a decoder for the
@@ -51,7 +73,31 @@ def asciiToString(asciiList):
 #       (Its inverse, the chr() function, is used in the provided helper)
 ######################################################################
 def caesarEncoder(str, shift):
-    pass    # TODO: replace this line with your code (multiple lines)
+
+    # Convert str to list of ASCII characters
+    asciiList = list(map(ord, str))
+
+    # Function to applyOffset to asciiList characters
+    def applyOffset(ch):
+        
+        # Ignore spaces
+        if ch == 32:
+            return ch
+
+        # Map alphabet to be from [0, 25] 
+        ch -= 65
+        # Apply offset, using mod 26 to wrap around the alphabet
+        ch = (ch + shift) % 26
+        # Convert back to ASCII
+        ch += 65
+
+        return ch
+
+    # Apply the offset to asciiList characters
+    encodedASCII = list(map(applyOffset, asciiList))
+    # Convert back to a string
+    encodedStr = asciiToString(encodedASCII)
+    return encodedStr
 
 ######################################################################
 # Decoder
@@ -61,4 +107,4 @@ def caesarEncoder(str, shift):
 # Hint: The chr() function converts ASCII to a single-character string
 ######################################################################
 def caesarDecoder(str, shift):
-    pass    # TODO: replace this line with your code (multiple lines)
+    return caesarEncoder(str, shift * -1)
