94cefb85f6924725ef7dd81862b2234ab7223924
Tyler S <42685071+Tyler-Seliber@users.noreply.github.com>
2021-11-14 21:35:18 -0500

    (n-1) call works

--- HW 4/hw4.py
+++ HW 4/hw4.py
@@ -1,3 +1,8 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Name    : Tyler Seliber
+# Pledge  : I pledge my honor that I have abided by the Stevens Honor System.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 from importlib import reload as Rfrsh
 import hmmm
 
@@ -7,54 +12,54 @@ import hmmm
 # The tests are still the same as from Lab 9
 # Tests: f(2) = 1, f(5) = 5, f(9) = 34
 RecFibSeq = """ # You may not need all lines
-00 
-01 
-02 
-03 
-04 
-05 
-06 
-07 
-08 
-09 
-10 
-11 
-12 
-13 
-14 
-15 
-16 
-17 
-18 
-19 
-20 
-21 
-22 
-23 
-24 
-25 
-26 
-27 
-28 
-29 
-30 
-31 
-32 
-33 
-34 
-35 
-36 
-37 
-38 
-39 
-40 
-41 
-42 
+00 setn r15 43
+01 read r1
+02 calln r14 5
+03 write r13
+04 halt
+05 jnezn r1 8
+06 setn r13 0
+07 jumpr r14
+08 addn r1 -1
+09 jnezn r1 12
+10 setn r13 1
+11 jumpr r14
+12 addn r1 1
+13 pushr r1 r15
+14 pushr r14 r15
+15 addn r1 -1
+16 calln r14 5
+17 popr r14 r15
+18 popr r1 r15
+19 add r13 r13 r1
+20 jumpr r14
+21 nop
+22 nop
+23 nop
+24 nop
+25 nop
+26 nop
+27 nop
+28 nop
+29 nop
+30 nop
+31 nop
+32 nop
+33 nop
+34 nop
+35 nop
+36 nop
+37 nop
+38 nop
+39 nop
+40 nop
+41 nop
+42 nop
 """
 
 # Change doDebug to false to turn off debugs
 runThis = RecFibSeq
-doDebug = True
+doDebug = False
 
 # Note: main() in the shell to easily reload
 def main(runArg=runThis,  debugArg=doDebug):
