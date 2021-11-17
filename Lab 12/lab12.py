62d7e7afa077e5cb4ca9f7032f52a09e0add0362
Tyler S <42685071+Tyler-Seliber@users.noreply.github.com>
2021-11-17 16:58:49 -0500

    Lab 12 setup

---
+++ Lab 12/lab12.py
@@ -0,0 +1,5 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Name    : Tyler Seliber
+# Pledge  : I pledge my honor that I have abided by the Stevens Honor System.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
